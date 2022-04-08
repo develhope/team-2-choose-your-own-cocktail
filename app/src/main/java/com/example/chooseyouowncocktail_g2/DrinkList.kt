46df59ee9dc7b9d1da73493a17754ca504567370
Francesco Anfuso <francescoanf96@gmail.com>
2022-04-08 17:45:59 +0200

    DrinkList fixed

--- app/src/main/java/com/example/chooseyouowncocktail_g2/DrinkList.kt
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/DrinkList.kt
@@ -1,24 +1,21 @@
 package com.example.chooseyouowncocktail_g2
 
-import android.graphics.drawable.Drawable
+import com.example.chooseyouowncocktail_g2.model.Beer
+import java.util.*
 
-data class Beer(
-    val name: String,
-    val description: String,
-    val shortDescription: String,
-    var img: Drawable?
-)
 
 object DrinkList {
 
-    private var beers = mutableListOf(
+    private var beers = listOf(
         Beer(
             "Ichnusa",
             "Una birra a bassa fermentazione con una gradazione alcolica di 4,7% vol." +
                     "Il colore è giallo tenue, quasi dorato, con una leggera effervescenza , limpida senza residui sul fondo." +
                     " Versata nel bicchiere ha una schiuma densa, bianca e consistente.",
             "Una birra a bassa fermentazione con una gradazione alcolica di 4,7% vol.",
-            null
+            66,
+            R.drawable.ichnusaprev,
+            false
         ),
         Beer(
             "Dreher",
@@ -27,37 +24,33 @@ object DrinkList {
                     "sprigiona una schiuma fine, persistente e compatta allo stesso tempo.Una birra leggera," +
                     "delicata con aromi di luppolo e cereali e note finali di miele.",
             "La Dreher è una birra a bassa fermentazione, quindi Lager.",
-            null
+            33,
+            R.drawable.dreherprev,
+            false
         ),
         Beer(
-            "Ichnusa",
+            "Nastro Azzurro",
             "La Nastro Azzurro è una Pilsner con una gradazione di 5.1%." +
                     "Il gusto di luppolo è gradevolmente amara, con una nota agrumata che la rende inconfondibile." +
                     "Versata nel bicchiere appare di un bel giallo paglierino limpido," +
                     "con una schiuma corposa e un deciso aroma di cereali e lieviti.",
             "La Nastro Azzurro è una Pilsner con una gradazione di 5.1%.",
-            null
+            33,
+            R.drawable.nastroazzurroprev,
+            false
         )
     )
 
-    fun getBeerList(): MutableList<Beer> {
+    fun beerList(): List<Beer> {
         return beers
     }
 
-    fun getName(id: Int): String {
-        return beers[id].name
-    }
-
-    fun getDesc(id: Int): String {
-        return beers[id].description
-    }
-
-    fun getShortDesc(id: Int): String {
-        return beers[id].shortDescription
-    }
-
-    fun setPreview(id: Int, img: Drawable) {
-        beers[id].img = img
+    fun setFavorite(beer: Beer, bool: Boolean) {
+        Collections.replaceAll(
+            beers,
+            beer,
+            beer.copy(favourite = bool)
+        )
     }
 
 }
---
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/model/Beer.kt
@@ -0,0 +1,12 @@
+package com.example.chooseyouowncocktail_g2.model
+
+import android.graphics.drawable.Drawable
+
+data class Beer(
+    val name: String,
+    val description: String,
+    val shortDescription: String,
+    val cl: Int,
+    val img: Int,
+    val favourite: Boolean
+)
