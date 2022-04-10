69422c897c6456c748d8aa6a5ebe862de9589ce7
Andrea Bulla <andreabulla71@gmail.com>
2022-04-10 21:00:25 +0200

    deatil page to show drink description

--- app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.chooseyouowncocktail_g2.R
 import com.example.chooseyouowncocktail_g2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,6 +36,14 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //val button = view.findViewById<Button>(R.id.buttonPanel)
+        binding.textHome.setOnClickListener {
+            findNavController().navigate(R.id.drinkDetailFragment, null)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "touch me to see detail page"
     }
     val text: LiveData<String> = _text
 }
