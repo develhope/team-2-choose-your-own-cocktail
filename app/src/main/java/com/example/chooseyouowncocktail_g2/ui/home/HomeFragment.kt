6c77ca5fe23319b18909bcbce85f4420dbfb7f3f
Andrea Bulla <andreabulla71@gmail.com>
2022-04-11 20:31:23 +0200

    Drink detail page new

--- app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
@@ -1,15 +1,18 @@
+
+
+
 package com.example.chooseyouowncocktail_g2.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.chooseyouowncocktail_g2.DrinkList
+import androidx.navigation.fragment.findNavController
 import com.example.chooseyouowncocktail_g2.R
+
 import com.example.chooseyouowncocktail_g2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -31,12 +34,18 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textHome.setOnClickListener {
+            findNavController().navigate(R.id.detailDrinkFragment, null)
+        }
     }
 
 
