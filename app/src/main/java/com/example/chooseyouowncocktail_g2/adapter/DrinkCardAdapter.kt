06db950d39a452ce090e2c2b6aa3623c72214b51
michele <mikechiave99@gmail.com>
2022-04-25 02:28:21 +0200

    Adapter and drinkcard fixed

---
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/adapter/DrinkCardAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.chooseyouowncocktail_g2.adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chooseyouowncocktail_g2.model.Beer
+import com.example.chooseyouowncocktail_g2.DrinkList
+import com.example.chooseyouowncocktail_g2.databinding.DrinkCardBinding
+
+class DrinkCardAdapter(
+    val beerListForAdapter: List<Beer>,
+) : RecyclerView.Adapter<DrinkCardAdapter.ViewHolder>() {
+    private lateinit var binding: DrinkCardBinding
+
+    inner class ViewHolder(val binding: DrinkCardBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = DrinkCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+      with(receiver = holder) {
+        with(beerListForAdapter[position]) {
+            binding.drinkName.text = this.name
+            binding.drinkCl.text = this.cl.toString()
+            binding.drinkShortDescription.text = this.shortDescription
+            binding.drinkImage.setImageResource(this.img)
+        }
+      }
+}
+
+override fun getItemCount(): Int {
+    return DrinkList.beerList().size
+}
+}
--- app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
@@ -11,7 +11,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.chooseyouowncocktail_g2.DrinkList
 import com.example.chooseyouowncocktail_g2.R
+import com.example.chooseyouowncocktail_g2.adapter.DrinkCardAdapter
 
 import com.example.chooseyouowncocktail_g2.databinding.FragmentHomeBinding
 
@@ -32,6 +34,8 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val drinkCardAdapter = DrinkCardAdapter(DrinkList.beerList())
+        binding.drinkCardRecyclerView.adapter = drinkCardAdapter
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
