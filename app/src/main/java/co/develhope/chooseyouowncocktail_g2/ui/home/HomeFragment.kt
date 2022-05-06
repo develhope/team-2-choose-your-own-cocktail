e0ccec40dadf72884b6e442b0d877a5ea6a34ff2
michele <mikechiave99@gmail.com>
2022-05-06 21:33:07 +0200

    Top of recyclerview

--- app/src/main/java/co/develhope/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
+++ app/src/main/java/co/develhope/chooseyouowncocktail_g2/ui/home/HomeFragment.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import co.develhope.chooseyouowncocktail_g2.DrinkList
 import co.develhope.chooseyouowncocktail_g2.adapter.DrinkCardAdapter
-import co.develhope.chooseyouowncocktail_g2.R
-
+import co.develhope.chooseyouowncocktail_g2.adapter.HeaderAdapter
 import co.develhope.chooseyouowncocktail_g2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -31,9 +29,12 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val headerAdapter = HeaderAdapter()
         val drinkCardAdapter = DrinkCardAdapter(DrinkList.beerList())
+        val concatAdapter = ConcatAdapter(headerAdapter, drinkCardAdapter)
 
-        binding.drinkCardRecyclerView.adapter = drinkCardAdapter
+
+        binding.drinkCardRecyclerView.adapter = concatAdapter
         val root: View = binding.root
 
         return root
