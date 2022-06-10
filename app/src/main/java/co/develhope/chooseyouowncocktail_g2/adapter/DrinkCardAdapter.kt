97b0ff365e260709fe4bb57f2ce3c14d2f1b5989
TenzoJackykama <andreabulla71@gmail.com>
2022-06-10 15:16:46 +0200

    Fix Warning specific notifyadapter in private fun changeUnderlingAdapterListUnSetSaves(positionToRemove: Int) and private fun showPreferiteRecycleView

--- app/src/main/java/co/develhope/chooseyouowncocktail_g2/adapter/DrinkCardAdapter.kt
+++ app/src/main/java/co/develhope/chooseyouowncocktail_g2/adapter/DrinkCardAdapter.kt
@@ -8,6 +8,7 @@ import co.develhope.chooseyouowncocktail_g2.domain.model.Drink
 import co.develhope.chooseyouowncocktail_g2.setImageByUrl
 import android.content.Context
 import android.util.Log
+import android.view.View
 import co.develhope.chooseyouowncocktail_g2.DrinkList
 import co.develhope.chooseyouowncocktail_g2.R
 import kotlin.collections.indexOf as indexOf
@@ -41,7 +42,7 @@ class DrinkCardAdapter(private var drinkListForAdapter: List<Drink>,
                 binding.drinkName.text = this.name
 
                 binding.drinkShortDescription.text = this.shortDescription
-                showPreferiteRecycleView(this)
+                showPreferiteRecycleView(this, holder)
 
                 binding.drinkImage.setImageByUrl(
                     "https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg",
@@ -55,7 +56,7 @@ class DrinkCardAdapter(private var drinkListForAdapter: List<Drink>,
 
                binding.drinkFavourite.setOnClickListener {
                    this.favourite = !this.favourite
-                   showPreferiteRecycleView(this)
+                   showPreferiteRecycleView(this, holder)
                    action(DrinkAction.SetPref(this, this.favourite))
                    if(this.favourite)
                        changeUnderlingAdapterListSetSaves(position, 0)
@@ -71,25 +72,26 @@ class DrinkCardAdapter(private var drinkListForAdapter: List<Drink>,
     private fun changeUnderlingAdapterListSetSaves(toReomve: Int, toInsert: Int){
         val drink =(drinkListForAdapter as ArrayList<Drink>).get(toReomve)
         (drinkListForAdapter as ArrayList<Drink>).remove(drink)
+        this.notifyItemRemoved(toReomve)
         (drinkListForAdapter as ArrayList<Drink>).add(toInsert, drink)
-        this.notifyDataSetChanged()
+        this.notifyItemInserted(toInsert)
     }
 
     private fun changeUnderlingAdapterListUnSetSaves(positionToRemove: Int){
         val drink = (drinkListForAdapter as ArrayList<Drink>)[positionToRemove]
         val originIndexToInsert = DrinkList.drinkList().indexOf(DrinkList.drinkList().filter { it.name == drink.name }.get(0))
         (drinkListForAdapter as MutableList<Drink>).remove(drink)
+        this.notifyItemRemoved(positionToRemove)
         (drinkListForAdapter as MutableList<Drink>).add(originIndexToInsert, drink)
-        this.notifyDataSetChanged()
-
+        this.notifyItemInserted(originIndexToInsert)
     }
 
-    private fun showPreferiteRecycleView(drinkCard: Drink){
-        if (drinkCard.favourite){
-            binding.drinkFavourite.setBackgroundResource(R.drawable.ic_fav_on)
+    private fun showPreferiteRecycleView(drink: Drink, holder: ViewHolder){
+        if (drink.favourite){
+            holder.binding.drinkFavourite.setBackgroundResource(R.drawable.ic_fav_on)
 
         }else{
-           binding.drinkFavourite.setBackgroundResource(R.drawable.ic_fav_off)
+            holder.binding.drinkFavourite.setBackgroundResource(R.drawable.ic_fav_off)
         }
     }
 
--- app/src/main/java/co/develhope/chooseyouowncocktail_g2/ui/search/SearchFragment.kt
+++ app/src/main/java/co/develhope/chooseyouowncocktail_g2/ui/search/SearchFragment.kt
@@ -1,7 +1,6 @@
 package co.develhope.chooseyouowncocktail_g2.ui.search
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
